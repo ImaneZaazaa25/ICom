c7c7579c74a8e8a1c85f6e74a22831f04f41407c
imane <zaazaaimane24@gmail.com>
2026-02-19 22:33:54 +0000

    First commit

---
+++ src/main/java/com/ICom/Icom/IcomApplication.java
@@ -0,0 +1,13 @@
+package com.ICom.Icom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ICom/Icom/IcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ICom.Icom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
