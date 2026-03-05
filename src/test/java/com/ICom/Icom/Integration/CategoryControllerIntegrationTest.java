03c635fb518ac56c6fb0ac313ffacccf2e3f013b
imane <zaazaaimane24@gmail.com>
2026-03-05 13:48:53 +0000

    Ajout du base h2 pour les testes d integration

--- src/test/java/com/ICom/Icom/Integration/CategoryControllerIntegrationTest.java
+++ src/test/java/com/ICom/Icom/Integration/CategoryControllerIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.ICom.Icom.Integration;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -12,6 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 class CategoryControllerIntegrationTest {
 
     @Autowired
@@ -27,9 +31,9 @@ class CategoryControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                         {
-                            "nom": "Admin",
+                            "nom": "Admina",
                             "prenom": "Test",
-                            "username": "adminCategory",
+                            "username": "adminaCategory",
                             "email": "adminCategory@test.com",
                             "motdepasse": "admin123",
                             "role": "Admin",
@@ -52,7 +56,7 @@ class CategoryControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                         {
-                            "username": "adminCategory",
+                            "username": "adminaCategory",
                             "motdepasse": "admin123"
                         }
                         """))
--- src/test/java/com/ICom/Icom/Integration/JwtIntegrationTest.java
+++ src/test/java/com/ICom/Icom/Integration/JwtIntegrationTest.java
@@ -1,16 +1,20 @@
 package com.ICom.Icom.Integration;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 class JwtIntegrationTest {
 
     @Autowired
