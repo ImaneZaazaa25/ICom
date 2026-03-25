5c8d60f99114020c5a7cae01ce8b83efdf601dd1
Zakariae <nassirizakariae113@gmail.com>
2026-03-25 20:37:08 +0100

    modifying the bugs

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -13,11 +13,13 @@ const Button = ({ children, onClick, type = "button", className = "", disabled =
     </button>
   );
 };
-Button.prototypes = {
+
+Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
   type: PropTypes.string,
   className: PropTypes.string,
   disabled: PropTypes.bool,
-}
+};
+
 export default Button;
