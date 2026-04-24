6e55a754ba944bd542dcf5b175807430f529944c
Hajar Bensalah <bensalahhajar139@gmail.com>
2026-04-24 12:18:33 +0000

    fixing sonar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,38 +9,30 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
 
-    // ✅ IMPORTANT : exclure Selenium
     exclude: [
       '**/node_modules/**',
       '**/tests/selenium/**'
     ],
 
-    coverage: {
-      provider: 'v8',
-      reporter: ['lcov', 'text', 'html'],
-      include: [
-        'src/components/common/**/*.{js,jsx}',
-        'src/components/layout/**/*.{js,jsx}',
-        'src/components/product/**/*.{js,jsx}',
-        'src/context/CartContext.jsx',
-        'src/hooks/useCart.js',
-        'src/hooks/useCarousel.js',
-        'src/hooks/useCategories.js',
-        'src/hooks/useFilters.js',
-        'src/hooks/useProducts.js',
-        'src/pages/Cart/**/*.{js,jsx}',
-        'src/pages/Home/**/*.{js,jsx}',
-        'src/pages/Login/**/*.{js,jsx}',
-        'src/pages/Orders/**/*.{js,jsx}',
-        'src/pages/ProductDetails/**/*.{js,jsx}',
-        'src/pages/Products/**/*.{js,jsx}',
-        'src/pages/Profile/**/*.{js,jsx}',
-        'src/pages/Register/**/*.{js,jsx}',
-        'src/pages/Admin/**/*.{js,jsx}',
-        'src/utils/filterProducts.js',
-        'src/utils/formatPrice.js',
-      ],
-      exclude: [],
-    },
+   coverage: {
+  provider: 'v8',
+  reporter: ['text', 'lcov', 'html'],
+
+  all: false, // 🔥 IMPORTANT
+
+  include: [
+    'src/**/*.{js,jsx}'
+  ],
+
+  exclude: [
+    '**/node_modules/**',
+    '**/*.css',
+    '**/tests/**',
+    '**/__tests__/**',
+    '**/setupTests.js',
+    'src/main.jsx',
+    'src/App.jsx'
+  ],
+},
   },
 })
